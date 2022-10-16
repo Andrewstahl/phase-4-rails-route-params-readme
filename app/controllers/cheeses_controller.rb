54f6352ac7e8d87d4b4f018eb13635db2884208a
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-16 12:34:18 -0400

    All files working

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,12 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    # byebug
+    # Find the cheese using the ID from the URL
+    cheese = Cheese.find(params[:id])
+    # Return a JSON response
+    render json: cheese
+  end
+
 end
